77cdb3e93524f42f29e429460271dbfd538b7fab
Stephen Wiss <smwiss326@gmail.com>
2016-11-06 16:36:22 -0600

    fix

--- myapp/routes/userprofile.js
+++ myapp/routes/userprofile.js
@@ -1,12 +1,24 @@
 var express = require('express');
 var router = express.Router();
+mysql = require('mysql');
 
-/* GET home page. */
+var connection = mysql.createConnection({
+  host: 'mysql.eecs.ku.edu',
+  user: 'swiss',
+  password: 'Password123!',
+  database: 'swiss',
+});
 router.get('/', function(req, res, next) {
   var user;
   if(req.user != undefined)
     user = req.user[0].User_Id;
-  res.render('Profile/userprofile', { title: "Profile", user: user });
+    var sqlq = "SELECT  FROM Users WHERE User_Id = " + "\'" + user + "\'";
+    console.log(sqlq);
+    connection.query(sqlq, function(err, results) {
+      console.log(JSON.stringify(results));
+      console.log(sqlq);
+      res.render('Profile/userprofile', { title: "Profile", user: user });
+    });
 });
 
 module.exports = router;
