ae02df97f6414b5825a3567f0dd0f70f0cbbe1dd
Omar Alzubbi <o299a438@ku.edu>
2016-11-06 11:45:00 -0600

    Querying databases, etc.

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
+mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: 'mysql.eecs.ku.edu',
+  user: 'swiss',
+  password: 'Password123!',
+  database: 'swiss',
+});
+var htmlH = "<outerdiv><div id = \"postContent \">";
+var html = ""
+var htmlL ="</div></outerdiv>"
+var sqlq = "SELECT * FROM Posts";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'FED PROJECT' });
+  connection.query(sqlq, function(err, results) {
+    console.log(JSON.stringify(results));
+    /*resultVar = JSON.stringify(results);
+    var lastChar = '\0';
+    var inQuotes = false;
+    [{"post_id":2,"content":"this a post","author_id":"wtf"},{"post_id":3,"content":"this a post","author_id":"wtf"},
+    for(var i = 0;i < resultVar.length;i++){
+      if(resultVar[i] == '{' && (lastChar == '[' || lastChar == ',') && !inQuotes){
+        html += "<div>";
+      }
+        //html += "<div>" + JSON.stringify(JSONres[i]) + "</div>";
+        lastChar = resultVar[i];
+    }*/
+    var htmlFin = htmlH + html + htmlL;
+    console.log(html);
+    res.render('post_results', {results: html, title: "Post Results" });
+  });
+//  res.render('index', { title: 'FED PROJECT',results: html });
 });
 
 module.exports = router;
